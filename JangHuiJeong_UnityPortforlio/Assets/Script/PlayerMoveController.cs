b1f18fef8dfab0c03da43808f4bfa1f6f0c157a0
Lyeo-dun <61930613+Lyeo-dun@users.noreply.github.com>
2021-10-07 15:24:02 +0900

    Edit Player
    Jump & Rotate

--- JangHuiJeong_UnityPortforlio/Assets/Script/PlayerMoveController.cs
+++ JangHuiJeong_UnityPortforlio/Assets/Script/PlayerMoveController.cs
@@ -13,19 +13,35 @@ public class PlayerMoveController : MonoBehaviour
     [SerializeField] private GameObject FlashLight;
     private bool isFlash;
 
+    // ** 점프 구현
+    private bool Jumping;
+    private bool Drop;
+    private float JumpSpeed;
+    private float OldPositionY;
+    private float MaximumY;
+
     private void Awake()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         MainCamera = Camera.main.gameObject;
         FlashLight = GameObject.Find("FlashLight");
     }
 
     void Start()
-    {
+    {        
         MoveSpeed = 5.0f;
         RotateSpeed = 3.5f;
 
         FlashLight.SetActive(false);
         isFlash = FlashLight.activeSelf;
+
+        CameraAngle = 0.0f;
+
+        Jumping = false;
+        Drop = false;
+        JumpSpeed = 0.0f;
+        OldPositionY = transform.position.y;
+        MaximumY = 5.0f;
     }
 
     void Update()
@@ -33,10 +49,30 @@ public class PlayerMoveController : MonoBehaviour
         // ** 플레이어 이동
         float Hor = Input.GetAxisRaw("Horizontal");
         float Ver = Input.GetAxisRaw("Vertical");
-        transform.Translate(Hor * MoveSpeed * Time.deltaTime, 0.0f, Ver * MoveSpeed * Time.deltaTime);
+        
+        // ** 플레이어가 갈 방향으로 레이를 쏜 후, 충돌물체와 가깝다면 움직이지 않도록 한다.
+        // ** NavMash나 Collider로만 막았던 결과, 플레이어가 가고 싶은 곳으로 가지 못하도록 충돌하게 되면 플레이어가 떨리는 현상이 발생하여 추후 플레이어를 조작하지 않음에도
+        // ** 플레이어가 돌아가는 현상을 발견하여 현상을 막기 위해 레이를 쏘고 충돌물체와 일정 거리가 되면 움직이지 않도록 함
+        {
+            float ObstacleMinDistance = 1.0f;
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward * Ver), out hit, Mathf.Infinity))
+            {
+                if (Vector3.Distance(hit.point, transform.position) > ObstacleMinDistance)
+                {
+                    transform.Translate(0.0f, 0.0f, Ver * MoveSpeed * Time.deltaTime);                   
+                }
+            }
+            
+            if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right * Hor), out hit, Mathf.Infinity))
+            {
+                if (Vector3.Distance(hit.point, transform.position) > ObstacleMinDistance)
+                {
+                    transform.Translate(Hor * MoveSpeed * Time.deltaTime, 0.0f, 0.0f);
+                }
+            }
+        }
 
-        // ** 시작했을 때 마우스가 갑자기 어디론가 팍 튀어버리는 현상이 있음
-        // ** 우선 현상 방지를 위해 우클릭을 하고 있을 시에만 시야 회전
         if(Input.GetMouseButton(1))
             PlayerRotate();
 
@@ -60,6 +96,36 @@ public class PlayerMoveController : MonoBehaviour
             isFlash = !isFlash;
             FlashLight.SetActive(isFlash);
         }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            Jump();
+
+        if(Jumping)
+        {
+            if(Drop)
+                transform.Translate(Vector3.up * -JumpSpeed * Time.deltaTime);
+            else
+            {
+                transform.Translate(Vector3.up * JumpSpeed * Time.deltaTime);
+
+                if (MaximumY + OldPositionY <= transform.position.y)
+                {
+                    Drop = true;
+                }
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(Jumping)
+            if(Drop)
+                if(collision.gameObject)
+                    Jumping = false;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag != "Ground")
+            Jumping = true;
     }
 
     // ** 마우스 위치에 따라 플레이어가 그 방향을 보게 만듦
@@ -71,7 +137,21 @@ public class PlayerMoveController : MonoBehaviour
         float MouseY = Input.GetAxis("Mouse Y");
         CameraAngle -= MouseY * RotateSpeed;
         CameraAngle = Mathf.Clamp(CameraAngle, -90, 90);
+
         // ** 부모가 있는 경우 local로 변경
-        MainCamera.transform.localEulerAngles = Vector3.right * CameraAngle;
+        MainCamera.transform.localEulerAngles = Vector3.right * CameraAngle;    
     }
+
+    // ** 점프값 셋팅
+    void Jump()
+    {
+        if (Jumping)
+            return;
+
+        Jumping = true;
+        Drop = false;
+        JumpSpeed = 8.0f;
+        OldPositionY = transform.position.y;
+    }
+    
 }
